260aee1f58f79fa6748de16f388c5c56d76d083c
Chase Simmonds <chasexsimmonds@gmail.com>
2022-06-17 18:32:41 +0000

    stock cards styled correctly

--- js/main.js
+++ js/main.js
@@ -28,6 +28,8 @@ function getStocks() {
       var $li = document.createElement('li');
       $ul.appendChild($li);
 
+      // Ticker
+
       var $ticker = document.createElement('h2');
       $li.appendChild($ticker);
       $ticker.textContent = xhr.response[i].ticker;
@@ -35,17 +37,55 @@ function getStocks() {
       var $line = document.createElement('hr');
       $li.appendChild($line);
 
-      var $sentiment = document.createElement('p');
-      $li.appendChild($sentiment);
-      $sentiment.textContent = 'Sentiment: ' + xhr.response[i].sentiment;
+      var $div1 = document.createElement('div');
+      $li.appendChild($div1);
 
-      var $score = document.createElement('p');
-      $li.appendChild($score);
-      $score.textContent = 'Score: ' + xhr.response[i].sentiment_score;
+      // Bullish or Bearish
 
-      var $comments = document.createElement('p');
-      $li.appendChild($comments);
-      $comments.textContent = 'Comments: ' + xhr.response[i].no_of_comments;
+      var $sentiment = document.createElement('text-content');
+      var $span = document.createElement('span');
+
+      var $sentimentData = xhr.response[i].sentiment;
+      $div1.appendChild($sentiment);
+      $div1.appendChild($span);
+      $sentiment.textContent = 'Sentiment: ';
+      $span.textContent = $sentimentData;
+      if ($span.textContent === 'Bullish') {
+        $span.setAttribute('class', 'green');
+      } else {
+        $span.setAttribute('class', 'red');
+      }
+
+      // Sentiment Score
+
+      var $div2 = document.createElement('div');
+      $div1.appendChild($div2);
+
+      var $score = document.createElement('text-content');
+      var $span2 = document.createElement('span');
+
+      var $sentimentScoreData = xhr.response[i].sentiment_score;
+      $div2.appendChild($score);
+      $div2.appendChild($span2);
+      $score.textContent = 'Score: ';
+      $span2.textContent = $sentimentScoreData;
+      $span2.setAttribute('class', 'bold');
+
+      // Comments
+
+      var $div3 = document.createElement('div');
+      $div2.appendChild($div3);
+
+      var $comments = document.createElement('text-content');
+      var $span3 = document.createElement('span');
+
+      var $commentsData = xhr.response[i].no_of_comments;
+      $div3.appendChild($comments);
+      $div3.appendChild($span3);
+      $comments.textContent = 'Comments: ';
+      $span3.textContent = $commentsData;
+      $span3.setAttribute('class', 'bold');
+
     }
   });
   xhr.send();
